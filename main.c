1b6124abdd36fc743f82c92cc7a21115f7e8da08
Al-27 <125827852+Al-27@users.noreply.github.com>
2023-07-27 02:12:34 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,18 +2,21 @@
 #include "main.h"
 #include "shell.h"
 char* filename = null;
+char* shellFile = null;
 int commands_run = 1;
 int testing  = 0;
 
 int main(int __attribute__((unused)) argc, const char __attribute__((unused)) *argv[], char __attribute__((unused)) *envp[])
 {
     filename = strdup(argv[0]);
-    
+    testing = argc > 1 ? 1 : 0;
     if( isatty(STDIN_FILENO) && !testing)
         interactive();
     else 
+    {
+        shellFile = strdup(argv[testing]);
         noninteractive();
-         
+    }    
     free(filename);
     return 0;
 }
